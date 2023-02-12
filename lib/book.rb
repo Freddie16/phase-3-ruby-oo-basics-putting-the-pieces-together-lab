fee1ea3a528a5ceafdb706b106aa85c1ecf2d853
Fred Murigi <freddiemurigi@gmail.com>
2023-02-12 19:17:32 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title, author ="Agatha Christie")
+        @title = title
+        @author = author
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
